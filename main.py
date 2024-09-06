df3c6dd93ace656e9ca628f57537e0af08c10c67
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-06 08:51:29 +0530

    check

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import speech_recognition as sr
+import os
+from win32com.client import Dispatch
+
+speak = Dispatch("SAPI.SpVoice").Speak
+
+def say(text) :
+    speak(text)
 
 if __name__ == '__main__':
     print('PyCharm')
+    say("Hello I am Jarvis A I")
