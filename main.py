0f021042353c51c05ecf694ee1f6da4a0d899a14
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-09 20:26:53 +0530

    open websites using Jarvis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 from win32com.client import Dispatch
 import soundfile
+import webbrowser
 
 speak = Dispatch("SAPI.SpVoice").Speak
 
@@ -10,14 +11,26 @@ def say(text) :
 def take_command():
     r=sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold=1
+        r.pause_threshold=0.8
         audio=r.listen(source)
-        query=r.recognize_whisper(audio, language="english")
-        print(f"User said -  {query}")
+        try:
+            query = r.recognize_whisper(audio, language="english")
+            print(f"User said -  {query}")
+            return query
+        except Exception as e:
+            return "Please try again"
 
 if __name__ == '__main__':
     print("PyCharm")
     print("Speaking...")
     say("Hello I am Jarvis A I")
-    print("Listening...")
-    take_command()
+    while True:
+        print("Listening...")
+        query=take_command()
+        sites=[["youtube", "https://www.youtube.com"], ["google", "https://www.google.com"], ["wikipedia", "https://www.wikipedia.com"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]}")
+                webbrowser.open(site[1])
+
+        # say(query)
